24742fe0979488f709b647216331542e6898b92d
Zukzuk <dave.timmerman@gmail.com>
2025-12-01 19:02:57 +0100

    chore(release): 1.30.1

--- playnite/SyncniteBridge/Version.g.cs
+++ playnite/SyncniteBridge/Version.g.cs
@@ -3,6 +3,6 @@ namespace SyncniteBridge
 {
     internal static class BridgeVersion
     {
-        public const string Current = "1.30.0";
+        public const string Current = "1.30.1";
     }
 }
